cb44358e77d1473b898467d7166f379e6166781c
Mihail Maruhin <maruhin.mihail2@wb.ru>
2025-10-24 16:09:32 +0300

    update lib version 1.13->1.23

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -88,7 +88,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "ru.wildberries"
             artifactId = "analytics2.public"
-            version = "1.0.22"
+            version = "1.0.23"
 
             afterEvaluate {
                 from(components["release"])
