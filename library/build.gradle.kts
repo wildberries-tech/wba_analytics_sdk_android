fa2882933177a254577efbd964ced6f22e3f18ac
Mihail Maruhin <maruhin.mihail2@wb.ru>
2025-10-31 17:10:43 +0300

    incr and publish 1.0.24

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -88,7 +88,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "ru.wildberries"
             artifactId = "analytics2.public"
-            version = "1.0.23"
+            version = "1.0.24"
 
             afterEvaluate {
                 from(components["release"])
